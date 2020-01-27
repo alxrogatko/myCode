b7d04dbfa729832f314c7139db057654b5a8866a
alxrogatko <60095631+alxrogatko@users.noreply.github.com>
2020-01-27 22:11:33 +0300

    Update Linear40.java

--- src/by/htp/jd1/linear1/Linear40.java
+++ src/by/htp/jd1/linear1/Linear40.java
@@ -1,20 +1,21 @@
 package by.htp.jd1.linear1;
 
-//Дано значение х. Получить значения -2х + 3х2 - 4х3 и 1 + 2х + 3х2 + 4х3 . Позаботьтесь об экономии операций
+//Р”Р°РЅРѕ Р·РЅР°С‡РµРЅРёРµ С…. РџРѕР»СѓС‡РёС‚СЊ Р·РЅР°С‡РµРЅРёСЏ -2С… + 3С…2 - 4С…3 Рё 1 + 2С… + 3С…2 + 4С…3 . РџРѕР·Р°Р±РѕС‚СЊС‚РµСЃСЊ РѕР± СЌРєРѕРЅРѕРјРёРё РѕРїРµСЂР°С†РёР№
 import java.util.Scanner;
 
 public class Linear40 {
-	public static void main(String[] args) {
-		int x;
-		double rez1;
-		double rez2;
-		System.out.println("Введите x");
-		Scanner sc = new Scanner(System.in);
-		x = sc.nextInt();
-		rez1 = x * (x * (-4 * x + 3) - 2);
-		System.out.println("Результат 1" + rez1);
-		rez2 = x * (x * (4 * x + 3) + 2) + 1;
-		System.out.println("Результат 2" + rez2);
 
-	}
+    public static void main(String[] args) {
+        int x;
+        double rez1;
+        double rez2;
+        System.out.println("Р’РІРµРґРёС‚Рµ x");
+        Scanner sc = new Scanner(System.in);
+        x = sc.nextInt();
+        rez1 = x * (x * (-4 * x + 3) - 2);
+        System.out.println("Р РµР·СѓР»СЊС‚Р°С‚ 1" + rez1);
+        rez2 = x * (x * (4 * x + 3) + 2) + 1;
+        System.out.println("Р РµР·СѓР»СЊС‚Р°С‚ 2" + rez2);
+
+    }
 }
