6b7e6ad3f7c144fb702c91de0570a78240e5f822
alxrogatko <60095631+alxrogatko@users.noreply.github.com>
2020-01-27 22:12:48 +0300

    Update Linear5.java

--- src/by/htp/jd1/linear1/Linear5.java
+++ src/by/htp/jd1/linear1/Linear5.java
@@ -1,19 +1,19 @@
 package by.htp.jd1.linear1;
 
-//Составить алгоритм нахождения среднего арифметического двух чисел
+//РЎРѕСЃС‚Р°РІРёС‚СЊ Р°Р»РіРѕСЂРёС‚Рј РЅР°С…РѕР¶РґРµРЅРёСЏ СЃСЂРµРґРЅРµРіРѕ Р°СЂРёС„РјРµС‚РёС‡РµСЃРєРѕРіРѕ РґРІСѓС… С‡РёСЃРµР»
 import java.util.Scanner;
 
 public class Linear5 {
 
-	public static void main(String[] args) {
-		System.out.println("input numbers");
-		Scanner scan = new Scanner(System.in);
-		float a;
-		float b;
-		a = scan.nextFloat();
-		b = scan.nextFloat();
-		System.out.println("average=" + (a + b) / 2);
+    public static void main(String[] args) {
+        System.out.println("input numbers");
+        Scanner scan = new Scanner(System.in);
+        float a;
+        float b;
+        a = scan.nextFloat();
+        b = scan.nextFloat();
+        System.out.println("average=" + (a + b) / 2);
 
-	}
+    }
 
 }
