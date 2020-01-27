4b8851590ca978839d3d58ee6848e33ccb5c9139
alxrogatko <60095631+alxrogatko@users.noreply.github.com>
2020-01-27 21:33:35 +0300

    Update Linear27.java

--- src/by/htp/jd1/linear1/Linear27.java
+++ src/by/htp/jd1/linear1/Linear27.java
@@ -1,13 +1,13 @@
 package by.htp.jd1.linear1;
-/*Дано значение a. Не используя никаких функций и никаких операций, кроме умножения, получить значение а8 за
-три операции и а10 за четыре операции*/
+/*Р”Р°РЅРѕ Р·РЅР°С‡РµРЅРёРµ a. РќРµ РёСЃРїРѕР»СЊР·СѓСЏ РЅРёРєР°РєРёС… С„СѓРЅРєС†РёР№ Рё РЅРёРєР°РєРёС… РѕРїРµСЂР°С†РёР№, РєСЂРѕРјРµ СѓРјРЅРѕР¶РµРЅРёСЏ, РїРѕР»СѓС‡РёС‚СЊ Р·РЅР°С‡РµРЅРёРµ Р°8 Р·Р°
+С‚СЂРё РѕРїРµСЂР°С†РёРё Рё Р°10 Р·Р° С‡РµС‚С‹СЂРµ РѕРїРµСЂР°С†РёРё*/
 import java.util.Scanner;
 public class Linear27 {
 	public static void main(String[] args) {
 		int a;
 		int b;
 		int c;
-		System.out.println("Введите значение a");
+		System.out.println("Р’РІРµРґРёС‚Рµ Р·РЅР°С‡РµРЅРёРµ a");
 
 		Scanner scanner = new Scanner(System.in);
 		a = scanner.nextInt();
@@ -17,14 +17,14 @@ public class Linear27 {
 		
 		a = a * a;
 		System.out.println(a);
-		//System.out.println("Проверка ерез Math.pow"+(int)(Math.pow(b, 8)));
+		
 		c=b;
 		c=c*c;
 		c=c*c;
 		c=c*c;
 		c=c*b*b;
 		System.out.println(c);
-		//System.out.println("Поверка через Math.pow"+((int)Math.pow(b,10)));
+		
 
 
 
