09d0e28c7f23ee55d1bf8b8c9fa0e70f9a2d630c
alxrogatko <60095631+alxrogatko@users.noreply.github.com>
2020-01-27 21:39:51 +0300

    Update Linear3.java

--- src/by/htp/jd1/linear1/Linear3.java
+++ src/by/htp/jd1/linear1/Linear3.java
@@ -1,12 +1,12 @@
 package by.htp.jd1.linear1;
 
-// Найдите значение функции: z = 2 * x + ( y – 2 ) * 5
+// РќР°Р№РґРёС‚Рµ Р·РЅР°С‡РµРЅРёРµ С„СѓРЅРєС†РёРё:  z = 2 * x + ( y вЂ“ 2 ) * 5
 import java.util.Scanner;
 
 public class Linear3 {
 
 	public static void main(String[] args) {
-		System.out.println("Введите значения х и у");
+		System.out.println("Р’РІРµРґРёС‚Рµ Р·РЅР°С‡РµРЅРёСЏ С… Рё Сѓ ");
 		Scanner inputNumber = new Scanner(System.in);
 		float z;
 		float x;
