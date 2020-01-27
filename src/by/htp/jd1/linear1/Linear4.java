ff33a91fd2c662f3a05f94f996263f4c27147a64
alxrogatko <60095631+alxrogatko@users.noreply.github.com>
2020-01-27 22:09:45 +0300

    Update Linear4.java

--- src/by/htp/jd1/linear1/Linear4.java
+++ src/by/htp/jd1/linear1/Linear4.java
@@ -1,23 +1,23 @@
 package by.htp.jd1.linear1;
 
-//Найдите значение функции: z = ( (a – 3 ) * b / 2) + c
+//РќР°Р№РґРёС‚Рµ Р·РЅР°С‡РµРЅРёРµ С„СѓРЅРєС†РёРё: z = ( (a вЂ“ 3 ) * b / 2) + c
 import java.util.Scanner;
 
 public class Linear4 {
 
-	public static void main(String[] args) {
-		System.out.println("Input numbers");
-		Scanner numb = new Scanner(System.in);
-		float a;
-		float b;
-		float c;
-		float z;
-		a = numb.nextFloat();
-		b = numb.nextFloat();
-		c = numb.nextFloat();
-		z = ((a - 3) * b / 2) + c;
-		System.out.println("z=" + z);
+    public static void main(String[] args) {
+        System.out.println("Input numbers");
+        Scanner numb = new Scanner(System.in);
+        float a;
+        float b;
+        float c;
+        float z;
+        a = numb.nextFloat();
+        b = numb.nextFloat();
+        c = numb.nextFloat();
+        z = ((a - 3) * b / 2) + c;
+        System.out.println("z=" + z);
 
-	}
+    }
 
 }
