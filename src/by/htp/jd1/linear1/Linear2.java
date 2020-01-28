63077bd92a3d311b82642fc56bb2d4b045b6eae4
alxrogatko <60095631+alxrogatko@users.noreply.github.com>
2020-01-28 11:36:07 +0300

    Update Linear2.java

--- src/by/htp/jd1/linear1/Linear2.java
+++ src/by/htp/jd1/linear1/Linear2.java
@@ -1,5 +1,5 @@
 package by.htp.jd1.linear1;
-// 2. Найдите значение функции: с = 3 + а
+// 2. РќР°Р№РґРёС‚Рµ Р·РЅР°С‡РµРЅРёРµ С„СѓРЅРєС†РёРё: СЃ = 3 + Р°
 import java.util.Scanner;
 public class Linear2 {
 
@@ -9,7 +9,7 @@ public class Linear2 {
 		System.out.println("Input number");
 		Scanner inputNumbers = new Scanner(System.in);
 		a = inputNumbers.nextFloat();
-		// вариант System.out.print(3+a);
+		// РІР°СЂРёР°РЅС‚ System.out.print(3+a);
 		c = 3 + a;
 		System.out.print(c); 
 
